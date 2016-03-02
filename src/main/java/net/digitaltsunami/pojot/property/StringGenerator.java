4fd138f0463b4ed7865acf2697b1d9edac064dd0
Dan Hagberg <sweep@digitaltsunami.net>
2016-03-02 05:26:28 -0800

    Issue #6 Added unit tests for StringGenerator.

--- src/main/java/net/digitaltsunami/pojot/property/StringGenerator.java
+++ src/main/java/net/digitaltsunami/pojot/property/StringGenerator.java
@@ -6,14 +6,24 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Generates string values with variability to provide collision free string values for a minimum
  * of 52 invocations.  The number of collision free strings increases as requested string size
  * varies.
- *
  */
 public class StringGenerator {
     private static final String DATA = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final int DATA_LEN = DATA.length();
     private static final AtomicInteger index = new AtomicInteger(0);
 
+    /**
+     * Create and return a string of length size.  If size is < 1, then
+     * an empty string will be returned.
+     *
+     * @param size Non-negative number of characters to return.
+     * @return a string containing size number of characters.
+     */
     public static String getNextString(int size) {
+        if (size < 1) {
+            return "";
+        }
+
         int bIdx = index.getAndIncrement();
         StringBuilder value = new StringBuilder(size);
 
@@ -21,8 +31,7 @@ public class StringGenerator {
 
         if (pe < DATA_LEN) {
             value.append(DATA.substring(bIdx, pe));
-        }
-        else if (pe > DATA_LEN) {
+        } else if (pe > DATA_LEN) {
             value.append(DATA.substring(bIdx, DATA_LEN));
             int remaining = size - (DATA_LEN - bIdx);
             int multiples = remaining / DATA_LEN;
@@ -30,8 +39,7 @@ public class StringGenerator {
                 value.append(DATA.substring(bIdx));
             }
             value.append(DATA.substring(0, remaining % DATA_LEN));
-        }
-        else { // pe == DATA_LEN
+        } else { // pe == DATA_LEN
             value.append(DATA.substring(bIdx, DATA_LEN));
         }
 
@@ -40,9 +48,17 @@ public class StringGenerator {
 
     /**
      * Return the next character in the sequence.
+     *
      * @return non-null and non-empty Character value
      */
     public static Character getNextChar() {
         return DATA.charAt(index.getAndIncrement());
+    }
+
+    /**
+     * Reset the index to 0
+     */
+    protected static void resetIndex() {
+        index.set(0);
     }
 }
